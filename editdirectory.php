a50aa48a54febdab066b92707360d98d822cccc5
gl1tchy_af <fahad.com66.fk@gmail.com>
2022-08-08 22:40:40 +0600

    edit directory

--- editdirectory.php
+++ editdirectory.php
@@ -101,7 +101,7 @@ if (!$conn) {
 		<div class="row">
 			<div class="col-2"></div>
 			<div class="col-8">
-				<h1 class="text-center m-2 mb-4 p-1 border border-dark border-3 border-top-0 border-end-0 rounded">Insert Video</h1>
+				<h1 class="text-center m-2 mb-4 p-1 border border-dark border-3 border-top-0 border-end-0 rounded">Edit Video</h1>
 			</div>
 			<div class="col-2"></div>
 		</div>
@@ -132,64 +132,18 @@ if (!$conn) {
 if(isset($_POST["submit"]) && !empty($_POST["catagory"])){
 		$catagory = $_POST['catagory'];
 		$department = $_POST['department'];
-        $insert = "INSERT into directory (catagory, department) VALUES ('$catagory', '$department')";
-        $res = mysqli_query($conn, $insert);
-        if($insert){
-            echo "Video Added";
-            header('location:addDirectory.php');
-        }else{
-            echo "Error";
-        }
+        $id = $_GET['id'];
+        $insert = "UPDATE directory SET catagory= '$catagory', department='$department' WHERE id= '$id'";
+            $res = mysqli_query($conn, $insert);
+            if ($insert) {
+                echo "Event Edited Successfully";
+                header("location:addDirectory.php");
+            } else {
+                echo "Error";
+            }
 }else{
 }
-
 ?>
-		<div class="row">
-			<div class="col-2"></div>
-			<div class="col-8">
-				<h1 class="text-center m-2 mb-4 p-1 border border-dark border-3 border-top-0 border-end-0 rounded">View News</h1>
-			</div>
-			<div class="col-2"></div>
-		</div>
-		<div class="row">
-			<div class="col-1"></div>
-			<div class="col-10">
-				<div class="row table-responsive m-3">
-					<table class="table table-lg align-middle">
-						<thead class="table-dark">
-							<tr>
-								<th class="text-center border"><strong>S.No</strong></th>
-								<th class="text-center border"><strong>Category</strong></th>
-								<th class="text-center border"><strong>Department</strong></th>
-								<th class="text-center border"><strong>Action</strong></th>
-
-							</tr>
-						</thead>
-						<tbody>
-							<?php
-$count = 1;
-$sel_query = "Select * from directory ORDER BY id desc;";
-$result = mysqli_query($conn, $sel_query);
-while ($row = mysqli_fetch_assoc($result)) { ?>
-							<tr>
-								<td><?php echo $count; ?></td>
-								<td><?php echo $row["catagory"]; ?></td>
-								<td><?php echo $row["department"]; ?></td>
-								<td>
-									<a class="btn btn-danger" href="deleteDirectory.php?id=<?=$row["id"] ?>">Delete</a>
-									<a class="btn btn-danger" href="editdirectory.php?id=<?=$row["id"] ?>">Edit</a>
-								</td>
-							</tr>
-							<?php $count++;
-}
-?>
-						</tbody>
-					</table>
-				</div>
-			</div>
-			<div class="col-1"></div>
-		</div>
-	</div>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 	<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
